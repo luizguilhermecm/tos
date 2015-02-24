8261ab4d328bf6fbed961b525cce4931d428c92c
luizguilhermecm <luizgui@gmail.com>
2015-02-23 22:37:20 -0300

    Initial Commit

---
+++ tos/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  tos
+//
+//  Created by snk on 2/23/15.
+//  Copyright (c) 2015 snk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
